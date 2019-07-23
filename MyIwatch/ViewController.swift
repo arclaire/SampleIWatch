f5bc744b0d4a66040b8d5c74d8c8ab19f5932213
Lucy <yuki_lcy@yahoo.com>
2019-07-23 09:28:14 +0700

    First Commit

---
+++ MyIwatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyIwatch
+//
+//  Created by Lucy PermataSari on 23/07/19.
+//  Copyright © 2019 Lucy PermataSari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
